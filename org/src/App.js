dcff37d75a974c845d5e850a2d8bb988a4cbe9cb
Federico Carranza Zavalia <fedecarranzanz@gmail.com>
2023-08-04 15:18:48 -0300

    Components

--- org/src/App.js
+++ org/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from "./componentes/Header/Header"
+import Formulario from './componentes/Formulario/Formulario';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola mundo con React.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprende React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Formulario />
     </div>
   );
 }
